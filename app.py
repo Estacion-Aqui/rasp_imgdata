a9ba1eba42a452206abe8da1f1340bd8a3eef7bd
Igor Cruz <igor_cruz_avelino@hotmail.com>
2021-08-19 22:12:49 -0300

    get_test1
    primeiro deploy com um método get para teste

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask 
+from flask import request, jsonify
+
+app = Flask(__name__)
+
+spots = [
+    {'id': 0,
+     'plate': '',
+     'empty': True},
+     {'id': 1,
+     'plate': 'GBM4E01',
+     'empty': False}
+]
+
+@app.route('/easteregg')
+def hello():
+    return 'O Igão gostava da suco!'
+
+@app.route('/api/v1/resources/spots/all', methods=['GET'])
+def api_call():
+    return jsonify(spots)
+
+if __name__ == '__name__':
+    app.run(debug=True, host='0.0.0.0')
