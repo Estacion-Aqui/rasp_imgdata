0e1b69ec74c608675097dd2173fbcc259360d643
icrxz <cruxstyle77@gmail.com>
2021-08-19 22:49:06 -0300

    removing debug

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def hello():
 def api_call():
     return jsonify(spots)
 
-if __name__ == '__name__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
