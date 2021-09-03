81c38e787b27ddd9bf2e5471cc8f0195977c1664
Igor Cruz <igor_cruz_avelino@hotmail.com>
2021-09-03 18:15:44 -0300

    save image in image processor folder

--- img_converter.py
+++ img_converter.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, jsonify, os, base64, datetime
+from flask import Flask, request, jsonify
+import os, base64, datetime, fnmatch
 
 app = Flask(__name__)
+imgDir = ""
 
 @app.route('/api/imgdata', methods=['POST'])
 def img_convert():
-    filename = datetime.datetime.now().strftime("%H%M%S%d%m%Y") + ".png"
-
+    currentTime = datetime.datetime.now().strftime("%H%M%S%d%m%Y")
     img_data = request.get_json().get('img_data')
+    cam_id = request.get_json().get('cam_id')
+    spot_number = request.get_json().get('spot_number')
 
-    with open(filename, "wb") as fh:
+    filename = f"{cam_id}_{spot_number}_{currentTime}.png"
+
+    with open(os.path.join(imgDir, filename), "wb") as fh:
         fh.write(base64.b64decode(img_data))
 
     return "OK"
+
+for path, dirs, files in os.walk('..'):
+  for dir in fnmatch.filter(dirs, 'resources'):
+    imgDir = os.path.abspath(os.path.join(path, dir))
 
 app.run(host='0.0.0.0', port=5000)
