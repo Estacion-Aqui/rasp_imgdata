4a95d38954a3faf26515439c4200fff742f1b318
icrxz <cruxstyle77@gmail.com>
2021-11-09 11:10:13 -0300

    change save image

--- img_converter.py
+++ img_converter.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
-import os, base64, datetime, fnmatch
+from PIL import Image
+import os, base64, datetime, fnmatch, io, cv2
+import numpy as np
 
 app = Flask(__name__)
 imgDir = ""
@@ -11,10 +13,13 @@ def img_convert():
     cam_id = request.get_json().get('cam_id')
     spot_number = request.get_json().get('spot_number')
 
-    filename = f"{cam_id}_{spot_number}_{currentTime}.png"
-
-    with open(os.path.join(imgDir, filename), "wb") as fh:
-        fh.write(base64.b64decode(img_data))
+    filename = f"{cam_id}_{spot_number}_{currentTime}.jpeg"
+    filepath = os.path.join(imgDir, filename)
+    
+    nparr = np.frombuffer(base64.b64decode(img_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    cv2.imwrite(filepath, img)
 
     return "OK"
 
